8a017eb0ba657d4473385eb8c152bc568077ba8c
botechiadev <botechiadev@gmail.com>
2024-06-28 16:25:20 -0300

    setup completo

--- projetos-botechia-erika/projeto-labecommerce-backend/exercicios/process-argv.js
+++ projetos-botechia-erika/projeto-labecommerce-backend/exercicios/process-argv.js
@@ -1,10 +1,10 @@
-const arg = processs.argv[2]
-
-if(arg === "bananinha"){
-    console.log("node ex1.js")
-}
-else if (arg === "player") {
+const value = process.argv[2];
+const value1 = process.argv[3]
+console.log(value)
+if (value1 === "iniciado") {
+  console.log("node exercicios/ex1.js");
+} else if (value1 === "player") {
   console.log("node ex2.js");
-}else{
-    console.log("error")
+} else {
+  console.log("error");
 }
